f935e3d1a41c6b5804147d6e53ff26758bf54170
Luis Fernando Flores Padilla <lffp.prog@gmail.com>
2026-03-03 20:24:48 -0400

    First Update 03/03/2026 (devFerCh0)

--- ESAM.GrowTracking.Application/Services/UserSessionValidatorService.cs
+++ ESAM.GrowTracking.Application/Services/UserSessionValidatorService.cs
@@ -1,10 +1,9 @@
 ﻿using ESAM.GrowTracking.Application.Commons.Result;
+using ESAM.GrowTracking.Application.Commons.Types;
 using ESAM.GrowTracking.Application.Commons.ValueObjects;
 using ESAM.GrowTracking.Application.Interfaces.Infrastructure.Services;
 using ESAM.GrowTracking.Application.Interfaces.Percistence.Repositories;
 using ESAM.GrowTracking.Application.Interfaces.Services;
-using ESAM.GrowTracking.Domain.Entities;
-using System.Threading;
 
 namespace ESAM.GrowTracking.Application.Services
 {
@@ -13,13 +12,17 @@ namespace ESAM.GrowTracking.Application.Services
         private readonly IBlacklistedAccessTokenTemporaryRepository _blacklistedAccessTokenTemporaryRepository;
         private readonly IBlacklistedAccessTokenPermanentRepository _blacklistedAccessTokenPermanentRepository;
         private readonly IUserRepository _userRepository;
+        private readonly IDateTimeService _dateTimeService;
+        private readonly ICurrentUserService _currentUserService;
 
         public UserSessionValidatorService(IBlacklistedAccessTokenTemporaryRepository blacklistedAccessTokenTemporaryRepository, 
-            IBlacklistedAccessTokenPermanentRepository blacklistedAccessTokenPermanentRepository, IUserRepository userRepository)
+            IBlacklistedAccessTokenPermanentRepository blacklistedAccessTokenPermanentRepository, IUserRepository userRepository, IDateTimeService dateTimeService, ICurrentUserService currentUserService)
         {
             _blacklistedAccessTokenTemporaryRepository = blacklistedAccessTokenTemporaryRepository ?? throw new ArgumentNullException(nameof(blacklistedAccessTokenTemporaryRepository));
             _blacklistedAccessTokenPermanentRepository = blacklistedAccessTokenPermanentRepository ?? throw new ArgumentNullException(nameof(blacklistedAccessTokenPermanentRepository));
             _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _dateTimeService = dateTimeService ?? throw new ArgumentNullException(nameof(dateTimeService));
+            _currentUserService = currentUserService ?? throw new ArgumentNullException(nameof(currentUserService));
         }
 
         public async Task<Result> IsJtiBlacklistedAccessTokenTemporaryAsync(string jti, bool asTracking = false, CancellationToken cancellationToken = default)
@@ -38,14 +41,42 @@ namespace ESAM.GrowTracking.Application.Services
             return Result.Ok();
         }
 
-        public async Task<Result> IsUserAccountValidAsync(int idUsuario, string secutiryStamp, int tokenVersion, DateTime utcNow, bool asTracking = false, CancellationToken cancellationToken = default)
+        public async Task<Result> IsUserAccountValidAsync(int userId, string secutiryStamp, int tokenVersion, DateTime utcNow, bool asTracking = false, CancellationToken cancellationToken = default)
         {
-            var validateUserStatus = await _userRepository.ValidateUserStatusAsync(id: idUsuario, utcNow: utcNow, asTracking: asTracking, cancellationToken: cancellationToken);
+            var validateUserStatus = await _userRepository.ValidateUserStatusAsync(id: userId, utcNow: utcNow, asTracking: asTracking, cancellationToken: cancellationToken);
             if (!validateUserStatus)
                 return Result.Fail(ErrorValueObject.Unauthorized("Usuario inválido o bloqueado."));
-            var validateUserSecutiry = await _userRepository.ValidateUserSecurityAsync(id: idUsuario, securityStamp: secutiryStamp, tokenVersion: tokenVersion, asTracking: asTracking, cancellationToken: cancellationToken);
+            var validateUserSecutiry = await _userRepository.ValidateUserSecurityAsync(id: userId, securityStamp: secutiryStamp, tokenVersion: tokenVersion, asTracking: asTracking, cancellationToken: cancellationToken);
             if (!validateUserSecutiry)
                 return Result.Fail(ErrorValueObject.Unauthorized("Sesión invalidada por cambios en la cuenta."));
+            return Result.Ok();
+        }
+
+        public async Task<Result> ValidateAccessAsync(CancellationToken cancellationToken = default)
+        {
+            var accessTokenType = _currentUserService.AccessTokenType!.Value;
+            var jti = _currentUserService.Jti!;
+            var userId = _currentUserService.UserId!.Value;
+            var securityStamp = _currentUserService.SecurityStamp!;
+            var tokenVersion = _currentUserService.TokenVersion!.Value;
+            var utcNow = _dateTimeService.UtcNow;
+            var asTracking = false;
+            if (accessTokenType == AccessTokenType.Temporary)
+            {
+                var isJtiBlacklistedAccessTokenTemporaryResult = await IsJtiBlacklistedAccessTokenTemporaryAsync(jti: jti, asTracking: asTracking, cancellationToken: cancellationToken);
+                if (!isJtiBlacklistedAccessTokenTemporaryResult.IsSuccess)
+                    return isJtiBlacklistedAccessTokenTemporaryResult;
+            }
+            else
+            {
+                var isJtiBlacklistedAccessTokenPermanentResult = await IsJtiBlacklistedAccessTokenPermanentAsync(jti: jti, asTracking: asTracking, cancellationToken: cancellationToken);
+                if (!isJtiBlacklistedAccessTokenPermanentResult.IsSuccess)
+                    return isJtiBlacklistedAccessTokenPermanentResult;
+            }
+            var isUserAccountValidResult = await IsUserAccountValidAsync(userId: userId, secutiryStamp: securityStamp, tokenVersion: tokenVersion, utcNow: utcNow, asTracking: asTracking, 
+                cancellationToken: cancellationToken);
+            if (!isUserAccountValidResult.IsSuccess)
+                return isUserAccountValidResult;
             return Result.Ok();
         }
     }
